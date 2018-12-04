6bd24feeee795338ed525d66cdf521940844e402
Loukyminipig <623594876@qq.com>
2018-12-04 17:20:49 +0800

    Update lecture-detail.js

--- pages/lecture/lecture-detail/lecture-detail.js
+++ pages/lecture/lecture-detail/lecture-detail.js
@@ -1,5 +1,5 @@
 // pages/lecture/lecture-detail/lecture-detail.js
-import { DBPost } from '../../../cache/DBPost.js';
+var listDB = require('../../../cache/listDB.js');
 
 Page({
 
@@ -13,14 +13,11 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
- 
 
-  onLoad: function (options) {
-    var postId = options.id;
-    this.dbPost = new DBPost(postId);
-    this.postData = this.dbPost.getPostItemById().data;
+  //TODO : 这里的数据结构不是很好
+  onLoad: function(options) {
     this.setData({
-      post: this.postData
+      post: listDB.getListItemById(listDB.postListId.key_lecture, options.id)
     })
   },
 
@@ -29,49 +26,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
